0f95114b7e7514cad5fd167dc06a2b17ca91599a
Prashant Sehgal <prashantsehgal.95790@gmail.com>
2025-02-14 23:33:59 +0530

    feat: vercel speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import './globals.css'
 
 // vidstack styles
@@ -32,7 +33,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={openSans.variable}>{children}</body>
+      <body className={openSans.variable}>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   )
 }
