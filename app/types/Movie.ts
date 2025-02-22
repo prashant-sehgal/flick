21b72305305218cc4d7d3648a514cc89597ade8c
Prashant Sehgal <prashantsehgal.95790@gmail.com>
2025-02-22 14:51:24 +0530

    feat: added Movie Interface

---
+++ app/types/Movie.ts
@@ -0,0 +1,12 @@
+export default interface Movie {
+  id: string
+  title: string
+  description: string
+  duration: number
+  imdbRating: number
+  genres: string[]
+  releasedYear: number
+  card: string
+  poster: string
+  media: string
+}
