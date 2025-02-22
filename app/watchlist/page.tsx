85fc981f00905b2769da668d8ae3c1ec381e4ceb
Prashant Sehgal <prashantsehgal.95790@gmail.com>
2025-02-22 20:53:30 +0530

    feat: implemented /watchlist page

---
+++ app/watchlist/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './page.module.css'
+import MovieCard from '../components/MovieCard/MovieCard'
+
+export default function page() {
+  return (
+    <div className={styles.page}>
+      {Array.from({ length: 5 }).map((_, i) => (
+        <MovieCard width={8} key={i} />
+      ))}
+    </div>
+  )
+}
