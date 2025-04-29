4236e140ad5de68fa832f94ed9da539491cb0749
Prashant Sehgal <prashantsehgal.95790@gmail.com>
2025-04-29 16:10:05 +0530

    feat: docker files

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_API_URI:
+      process.env.NEXT_PUBLIC_API_URI ||
+      'https://flick-services-aecga7drcfdja0ad.francecentral-01.azurewebsites.net',
+    NEXT_PUBLIC_MEDIA_URI:
+      process.env.NEXT_PUBLIC_MEDIA_URI ||
+      'https://flickmediafiles.blob.core.windows.net/images<blobname>?sp=r&st=2025-04-29T10:20:09Z&se=2125-04-29T18:20:09Z&spr=https&sv=2024-11-04&sr=c&sig=ifgAOghjgjwqABehO%2FbSomrse7twsoowaK0jNqQQ1Xk%3D',
+  },
   images: {
     remotePatterns: [
       {
